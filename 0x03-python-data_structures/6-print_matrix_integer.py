d6a448919c5a44695eda0cf0762e9a2f8094caf4
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-08-12 19:19:03 +0300

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    ite = 0
+    for li in matrix:
+        for i in range(len(li)):
+            if i == (len(li) - 1):
+                print("{:d}".format(matrix[ite][i]), end="")
+            else:
+                print("{:d}".format(matrix[ite][i]), end=" ")
+        ite += 1
+        print()

3a26b01f01360f89ed6d1d74acf80e2792817e15
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-08-28 19:23:37 +0300

    fixing checker error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    new = my_list[:x]
     i = 0
-    for li in new:
-        print(li, end="")
-        i += 1
-    if x != 0:
-        print()
-    return i
-
-
-try:
-    safe_print_list()
-except NameError:
-    safe_print_list
+    while True:
+        try:
+            if i < x:
+                print(my_list[i], end="")
+                i += 1
+            else:
+                print()
+                return i
+        except IndexError:
+            print()
+            return i

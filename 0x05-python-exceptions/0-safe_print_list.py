a40040265838b5a8301dee65113792309b0c6b1e
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-08-28 18:52:35 +0300

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    new = my_list[:x]
+    i = 0
+    for li in new:
+        print(li, end="")
+        i += 1
+    if x != 0:
+        print()
+    return i
+
+
+try:
+    safe_print_list()
+except IndexError:
+    safe_print_list

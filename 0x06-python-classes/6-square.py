80461b5d570b754851aa24a8d529a9be990ccb6d
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-09-07 19:30:59 +0300

    fixing bugs

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,11 +46,11 @@ class Square:
     def my_print(self):
         ''' Print square using # '''
         if self.__size == 0:
-            print()
-        else:
-            for i in range(0, self.__size):
-                for sp in range(0, self.__position[0]):
-                    print(" ", end="")
-                for idx in range(0, self.__size):
-                    print("#", end="")
-                print()
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")

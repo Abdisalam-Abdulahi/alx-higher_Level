794a51130f442f3cae68fa00419783c4868e2b49
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-09-07 14:34:33 +0300

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+''' validate size '''
+
+
+class Square:
+    '''add size feild to the square class'''
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        ''' Returns size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' set size to value '''
+        if (isinstance(self.__size, int)) is False\
+                or (isinstance(value, int)) is False:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        ''' returns the area of the square '''
+        return self.__size * self.__size
+
+    def my_print(self):
+        ''' Print square using # '''
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for idx in range(0, self.__size):
+                    print("#", end="")
+                print()

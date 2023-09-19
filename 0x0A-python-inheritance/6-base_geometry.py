add7cef17379afda291b498e420cd25a4cbe1e49
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-09-19 19:04:28 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Geometry class
+'''
+
+
+class BaseGeometry:
+    '''
+    Geometry class
+    '''
+    def area(self):
+        ''' raise exception '''
+        raise Exception("area() is not implemented")

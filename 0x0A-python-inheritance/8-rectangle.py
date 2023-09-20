6f9929ca1ea02525f5f59edff78551ac3bc25d8b
Abdisalam Abdulahi <iidlex99@gmail.com>
2023-09-20 10:42:15 +0300

    importing a class from 7-base_geometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,21 +4,8 @@
 class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
 '''
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class BaseGeometry:
-    '''
-    Geometry class
-    '''
-    def area(self):
-        ''' raise exception '''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        ''' validates value '''
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
@@ -28,5 +15,5 @@ class Rectangle(BaseGeometry):
         ''' initilaize arguments'''
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, "width", self.__width)
-        BaseGeometry.integer_validator(self, "height", self.__height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
--- 0x0A-python-inheritance/test.py
+++ 0x0A-python-inheritance/test.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
+Rectangle = __import__('8-rectangle').Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-bg = BaseGeometry()
-
-try:
-    bg.area()
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+print(issubclass(Rectangle, BaseGeometry))
